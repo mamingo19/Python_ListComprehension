b722187be8857ffb271fa75f221a92f089b492ed
mamingo19 <huytton119@gmail.com>
2024-10-03 19:18:32 +0700

    implement exeption handling

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(data_dict)
 
-name = input("Enter your names or somethings IDK: ").upper()
+def generate():
+    name = input("Enter your names or somethings IDK: ").upper()
+    try:
+        result = [data_dict[letter] for letter in name]
+    except KeyError:
+        print("Please only LETTERS")
+        generate()
+    else:
+        print(result)
 
-result = [data_dict[letter] for letter in name]
-print(result)
+generate()
+
+#I did not know the code a function can run it own function in python skull*
